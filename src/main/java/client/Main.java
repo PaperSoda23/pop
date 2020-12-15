875e89603111734810e8085da49496dbf6ad9f93
Jack <eljukna@gmail.com>
2020-12-15 18:55:34 +0200

    setup console client

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,13 +1,19 @@
 package client;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    private static final String ip = "127.0.0.1";
+    private static final int port = 3333;
+
     public static void main(String[] args) {
-//        try {
-////            Client.connect("127.0.0.1", 3333);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            var scanner = new Scanner(System.in);
+            var client = new Client(ip, port);
+            client.connect(scanner::nextLine, System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

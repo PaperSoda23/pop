a460a9f22db697ca57637daa8b53e977942d974e
Jack <eljukna@gmail.com>
2020-12-17 14:38:21 +0200

    add request processor tests

---
+++ src/test/java/lt/papersoda/pop3/core/RequestProcessorTests.java
@@ -0,0 +1,47 @@
+package lt.papersoda.pop3.core;
+
+import lt.papersoda.pop3.pojo.Response;
+import lt.papersoda.pop3.user.UserSessionState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class RequestProcessorTests {
+    @Mock RequestParser mockRequestParser;
+    @Mock RequestHandler mockRequestHandler;
+    @Mock UserSessionState mockUserSessionState;
+    @Mock Response mockResponse;
+    private final String rawRequest = "request";
+
+    @InjectMocks
+    RequestProcessor requestProcessor;
+
+    @BeforeEach
+    public void setup() {
+        when(mockRequestHandler.handleClientRequest(any(), any())).thenReturn(mockResponse);
+    }
+
+    @Test
+    public void passes_raw_client_request_to_request_parser() {
+        requestProcessor.processClientRequest(rawRequest, mockUserSessionState);
+
+        verify(mockRequestParser).parseClientRequest(argThat(arg -> arg.equals(rawRequest)));
+    }
+
+    @Test
+    public void returns_response_received_from_request_handler() {
+        Response response = requestProcessor.processClientRequest(rawRequest, mockUserSessionState);
+
+        assertThat(response).isEqualTo(mockResponse);
+    }
+
+}
